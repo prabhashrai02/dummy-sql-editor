b32b675cd2be25c6004c81c87657ec43b497319c
Prabhash Rai <prabhashsahaj@gmail.com>
2023-07-21 15:06:42 +0530

    add home component

---
+++ src/Screen/Home/index.tsx
@@ -0,0 +1,11 @@
+import styles from './home.module.css';
+
+const Home = () => {
+    return(
+        <div className={styles.homepageContainer}>
+            HomePage
+        </div>
+    )
+}
+
+export default Home;
