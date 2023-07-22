37cc1138697a747c8b5f23596deb7b95a8f822bf
Prabhash Rai <prabhashsahaj@gmail.com>
2023-07-23 01:50:46 +0530

    add dummy table data

---
+++ src/Constants/table.ts
@@ -0,0 +1,91 @@
+export const table1 = {
+  data: [
+    ["John", "30", "Male", "USA", "Engineer", "Single", "New York", "Java", "Basketball", "Rock", "Coffee", "Sunny", "Beach", "Reading", "Tech", "Coding", "Action", "Pizza", "Summer", "Mountains", "Travel", "Photography", "Movies", "Gaming"],
+    ["Jane", "25", "Female", "Canada", "Teacher", "Married", "Toronto", "Python", "Football", "Pop", "Tea", "Rainy", "Mountains", "Movies", "Education", "Writing", "Romance", "Pasta", "Winter", "Beach", "Food", "Reading", "Hiking", "Art"],
+  ],
+  columnsHeader: ["Name", "Age", "Gender", "Country", "Occupation", "Marital Status", "City", "Programming Language", "Hobby", "Music Genre", "Drink", "Weather", "Location", "Interest", "Field", "Activity", "Style", "Favorite Food", "Season", "Destination", "Hobby Type", "Interest Area", "Favorite Movie Genre", "Game"]
+};
+
+export const table2 = {
+  data: [
+    ["John", "Hip Hop", "Professional", "New York", "USA", "Gold", "Sunny", "Basketball", "Coffee"],
+    ["Emily", "Ballet", "Intermediate", "Los Angeles", "USA", "Silver", "Rainy", "Swimming", "Tea"],
+  ],
+  columnsHeader: ["Dancer Name", "Style", "Level", "City", "Country", "Achievement", "Weather", "Favorite Sport", "Favorite Drink"]
+};
+
+export const table3 = {
+  data: [
+    ["Michael", "PC", "Action", "Strategy", "PlayStation", "Weekends", "Single Player", "Online", "FPS"],
+    ["Emma", "Console", "Adventure", "Racing", "Nintendo Switch", "Evenings", "Multiplayer", "Offline", "RPG"],
+  ],
+  columnsHeader: ["Gamer Name", "Preferred Platform", "Favorite Genre 1", "Favorite Genre 2", "Console of Choice", "Gaming Time", "Preferred Mode", "Online/Offline", "Favorite Game Type"]
+};
+
+export const table4 = {
+  data: [
+    ["John", "Beach", "Summer", "Bora Bora", "Tahiti", "Relaxing", "Seafood", "Snorkeling"],
+    ["Emily", "Mountain", "Winter", "Zermatt", "Switzerland", "Snowboarding", "Fondue", "Skiing"],
+  ],
+  columnsHeader: ["Traveler Name", "Destination Type", "Favorite Season", "Favorite Destination", "Country", "Activity", "Favorite Food"]
+};
+
+export const table5 = {
+  data: [
+    ["Ava", "Romance", "Fiction", "Jane Austen", "Pride and Prejudice", "Completed", "5", "England"],
+    ["Sophia", "Mystery", "Thriller", "Agatha Christie", "Murder on the Orient Express", "In Progress", "3", "USA"],
+  ],
+  columnsHeader: ["Reader Name", "Favorite Genre 1", "Favorite Genre 2", "Favorite Author", "Favorite Book", "Reading Status", "Rating (out of 5)", "Location"]
+};
+
+export const table6 = {
+  data: [
+    ["Virat Kohli", "Batsman", "32", "250", "12000", "70.00"],
+    ["Rohit Sharma", "Batsman", "34", "220", "11000", "60.00"],
+    ["Jasprit Bumrah", "Bowler", "27", "100", "150", "25.00"],
+    ["KL Rahul", "Batsman", "29", "180", "8000", "45.00"],
+    ["Ravindra Jadeja", "All-Rounder", "32", "180", "5000", "30.00"],
+    ["Shikhar Dhawan", "Batsman", "35", "200", "9000", "50.00"],
+    ["Mohammed Shami", "Bowler", "31", "120", "200", "28.00"],
+    ["Hardik Pandya", "All-Rounder", "27", "100", "3000", "35.00"],
+    ["Yuzvendra Chahal", "Bowler", "31", "80", "100", "15.00"],
+    ["Ajinkya Rahane", "Batsman", "33", "100", "4500", "40.00"],
+  ],
+  columnsHeader: ["Player Name", "Role", "Age", "Matches Played", "Runs Scored", "Batting Average"]
+};
+
+export const table7 = {
+  data: [
+    ["John", "Male", "25", "Engineer", "USA"],
+    ["Emily", "Female", "30", "Designer", "Canada"],
+    ["Michael", "Male", "28", "Doctor", "UK"],
+  ],
+  columnsHeader: ["Name", "Gender", "Age", "Occupation", "Country"]
+};
+
+export const table8 = {
+  data: [
+    ["Alice", "Female", "24", "Artist", "France"],
+    ["Bob", "Male", "27", "Developer", "Germany"],
+    ["Eva", "Female", "22", "Student", "Spain"],
+  ],
+  columnsHeader: ["Name", "Gender", "Age", "Occupation", "Country"]
+};
+
+export const table9 = {
+  data: [
+    ["Alex", "Male", "29", "Manager", "Australia"],
+    ["Sophie", "Female", "35", "Lawyer", "Italy"],
+    ["Daniel", "Male", "26", "Chef", "Russia"],
+  ],
+  columnsHeader: ["Name", "Gender", "Age", "Occupation", "Country"]
+};
+
+export const table10 = {
+  data: [
+    ["Mia", "Female", "23", "Teacher", "Japan"],
+    ["Henry", "Male", "31", "Writer", "Brazil"],
+    ["Isabella", "Female", "28", "Photographer", "Mexico"],
+  ],
+  columnsHeader: ["Name", "Gender", "Age", "Occupation", "Country"]
+};
