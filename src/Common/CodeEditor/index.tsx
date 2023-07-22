631b8244cc65c715dfc5c1151d86b68c0a19fd80
Prabhash Rai <prabhashsahaj@gmail.com>
2023-07-22 12:02:55 +0530

    add custom theme to code editor

--- src/Common/CodeEditor/index.tsx
+++ src/Common/CodeEditor/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 
+import InputDropdown from "../InputDropdown";
+
 import styles from "./codeEditor.module.css";
 
 const CodeEditor = () => {
   const [code, setCode] = useState<string>("");
+  const [selectedTheme, setSelectedTheme] = useState<string>("lightTheme");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const lineNumberRef = useRef<HTMLDivElement>(null);
 
@@ -26,25 +29,49 @@ const CodeEditor = () => {
     syncScroll();
   };
 
+  const handleThemeChange = (theme: string) => {
+    setSelectedTheme(theme);
+  };
+
   return (
-    <div className={`${styles.codeEditor} ${styles.lightTheme}`}>
-      <div ref={lineNumberRef} className={styles.lineNumbers}>
-        {code.split("\n").map((_, index) => (
-          <div key={index} className={styles.lineNumber}>
-            {index + 1}
-          </div>
-        ))}
+    <div>
+      <div className={styles.toolbar}>
+        <InputDropdown
+          label={""}
+          selectedOption={selectedTheme}
+          options={themeOptions}
+          onOptionChange={handleThemeChange}
+        />
       </div>
-      <textarea
-        ref={textAreaRef}
-        className={styles.codeTextArea}
-        value={code}
-        onChange={handleCodeChange}
-        onScroll={handleScroll}
-        placeholder="Enter your query here..."
-      />
+      <div className={`${styles.codeEditor} ${styles[selectedTheme]}`}>
+        <div ref={lineNumberRef} className={styles.lineNumbers}>
+          {code.split("\n").map((_, index) => (
+            <div key={index} className={styles.lineNumber}>
+              {index + 1}
+            </div>
+          ))}
+        </div>
+        <textarea
+          ref={textAreaRef}
+          className={styles.codeTextArea}
+          value={code}
+          onChange={handleCodeChange}
+          onScroll={handleScroll}
+          placeholder="Enter your query here..."
+        />
+      </div>
     </div>
   );
 };
+
+const themeOptions = [
+  { key: "Light Theme", value: "lightTheme" },
+  { key: "Dark Theme", value: "darkTheme" },
+  { key: "Night Owl Theme", value: "nightOwlTheme" },
+  { key: "Solarized Light Theme", value: "solarizedLightTheme" },
+  { key: "Dracula Theme", value: "draculaTheme" },
+  { key: "Oceanic Next Theme", value: "oceanicNextTheme" },
+  { key: "GitHub Theme", value: "gitHubTheme" },
+];
 
 export default CodeEditor;
