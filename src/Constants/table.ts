dd685d7917ac837c94e35dd19cd2ad60a0028b52
Prabhash Rai <prabhashsahaj@gmail.com>
2023-07-23 16:47:03 +0530

    moved json to assets

--- src/Constants/table.ts
+++ src/Constants/table.ts
@@ -1,13 +1,13 @@
-import table1Data from "./table1.json";
-import table2Data from "./table2.json";
-import table3Data from "./table3.json";
-import table4Data from "./table4.json";
-import table5Data from "./table5.json";
-import table6Data from "./table6.json";
-import table7Data from "./table7.json";
-import table8Data from "./table8.json";
-import table9Data from "./table9.json";
-import table10Data from "./table10.json";
+import table1Data from "../Assets/table1.json";
+import table2Data from "../Assets/table2.json";
+import table3Data from "../Assets/table3.json";
+import table4Data from "../Assets/table4.json";
+import table5Data from "../Assets/table5.json";
+import table6Data from "../Assets/table6.json";
+import table7Data from "../Assets/table7.json";
+import table8Data from "../Assets/table8.json";
+import table9Data from "../Assets/table9.json";
+import table10Data from "../Assets/table10.json";
 
 export const tables: QueryTable = {
   table1: table1Data,
