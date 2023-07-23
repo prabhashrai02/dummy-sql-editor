7607858843d4281ff4c2135f90b62c57f9ffe7b1
Prabhash Rai <prabhashsahaj@gmail.com>
2023-07-23 13:20:04 +0530

    fix table structure

--- src/Screen/Home/homeHelper.ts
+++ src/Screen/Home/homeHelper.ts
@@ -1,20 +1,13 @@
 
 import { sampleQueries } from "../../Constants/sampleQueries";
-import { table1, table2, table3, table4, table5 } from "../../Constants/table";
+import { QueryTable, tables } from "../../Constants/table";
 
-export const queryToTableMap: QueryTable = {
-  [sampleQueries[0]]: table1,
-  [sampleQueries[1]]: table2,
-  [sampleQueries[2]]: table3,
-  [sampleQueries[3]]: table4,
-  [sampleQueries[4]]: table5,
+export const sampleQuery: QueryTable = {
+  [sampleQueries[0]]: tables.table1,
+  [sampleQueries[1]]: tables.table2,
+  [sampleQueries[2]]: tables.table3,
+  [sampleQueries[3]]: tables.table4,
+  [sampleQueries[4]]: tables.table5,
 };
 
 export const queryHistory: QueryTable = {};
-
-type QueryTable = { [key: string]: Table };
-
-type Table = {
-  data: string[][];
-  columnsHeader: string[];
-}
