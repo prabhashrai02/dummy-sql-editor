b6a077f94eb6e51c61b4ee51e008b712dc0b541e
Prabhash Rai <prabhashsahaj@gmail.com>
2023-07-23 13:30:21 +0530

    add function for localStorage

---
+++ src/Utils/storageUtils.ts
@@ -0,0 +1,19 @@
+export const setLocalStorageData = <T>(key: string, data: T): void => {
+  try {
+    const serializedData = JSON.stringify(data);
+    localStorage.setItem(key, serializedData);
+  } catch (error) {
+    console.error("Error while setting data in local storage:", error);
+  }
+};
+
+export const getLocalStorageData = <T>(key: string): T | null => {
+  try {
+    const serializedData = localStorage.getItem(key);
+    if (serializedData === null) return null;
+    return JSON.parse(serializedData) as T;
+  } catch (error) {
+    console.error("Error while getting data from local storage:", error);
+    return null;
+  }
+};
